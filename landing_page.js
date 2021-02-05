7619a867a90ff3cab8a456f428fd694a7732d599
Kartik Yadav <kartikyadav94@gmail.com>
2021-02-05 15:17:33 +0530

    added solutions dropdown content

--- landing_page.js
+++ landing_page.js
@@ -1,44 +1,11 @@
+function display_block (input) {
+    input.style.display = "block"
+}
+function display_none (input) {
+    input.style.display ="none"
+}
+var pdt = document.getElementById("prdt");
 var dropdown_pdt = document.getElementById("header_dropdown_product");
-var pdt = document.getElementById("prdt")
-function drop_pdt() {
-    dropdown_pdt.style.display = "block";
-}
-prdt.addEventListener('click', drop_pdt)
-
-var rethink_p = document.getElementById("rethink_pricing");
-var automate_f = document.getElementById("automate_finops");
-var g_global = document.getElementById("go_global");
-var rethink_bttn = document.getElementById("rethink_btn");
-var automate_bttn = document.getElementById("automate_btn");
-var global_bttn = document.getElementById("global_btn");
-
-function rethink() {
-    if (rethink_pricing.style.display == "none"){
-        rethink_pricing.style.display = "block"
-        g_global.style.display == "none"
-        automate_f.style.display == "none"
-    }
-    else {
-        rethink_p.style.display == "none";
-    }
-}
-function automate() {
-    if (automate_f.style.display == "none"){
-        rethink_p.style.display == "none";
-        g_global.style.display == "none";
-        automate_f.style.display = "block";
-    }
-}
-function global() {
-    if (go_global.style.display == "none"){
-        rethink_p.style.display == "none";
-        automate_f.style.display == "none";
-        g_global.style.display == "block";
-    }
-    else {
-        go_global.style.display == "none";
-    }
-}
-rethink_bttn.addEventListener("click", rethink);
-automate_bttn.addEventListener("click", automate);
-global_bttn.addEventListener("click", global);
+pdt.addEventListener('mouseover', function(){
+    display_block(dropdown_pdt);
+})
